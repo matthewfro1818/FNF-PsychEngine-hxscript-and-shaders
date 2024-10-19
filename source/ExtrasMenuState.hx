1bf4e940b32256591ad24d81cbdcf71c9a4dd54a
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-19 08:53:31 -0400

    Update ExtrasMenuState.hx

--- source/ExtrasMenuState.hx
+++ source/ExtrasMenuState.hx
@@ -119,17 +119,17 @@ class ExtrasMenuState extends MusicBeatState
 		var modsbutton:FlxSprite = new FlxSprite(269, 241).loadGraphic(Paths.image('mainmenu/mods'));//Thanks to EIT for the tutoria
 		modsbutton.scrollFactor.set(0, 0);
 		modsbutton.flipX = false; //You should have already animated it in the right position in Animate
-		add(modsbutton);
+		menuItems.add(modsbutton);
 
 		var discordbutton:FlxSprite = new FlxSprite(269, 482).loadGraphic(Paths.image('mainmenu/discord'));//Thanks to EIT for the tutorial
 		discordbutton.scrollFactor.set(0, 0);
 		discordbutton.flipX = false; //You should have already animated it in the right position in Animate
-		add(discordbutton);
+		menuItems.add(discordbutton);
 
 		var creditsbutton:FlxSprite = new FlxSprite(269, 702).loadGraphic(Paths.image('mainmenu/credits'));//Thanks to EIT for the tutorial
 		creditsbutton.scrollFactor.set(0, 0);
 		creditsbutton.flipX = false; //You should have already animated it in the right position in Animate
-		add(creditsbutton);
+		menuItems.add(creditsbutton);
 
 	  var mchar:FlxSprite = new FlxSprite(238, 199).loadGraphic(Paths.image('backgrounds/$name'));
 		mchar.scrollFactor.set(0, 0);
